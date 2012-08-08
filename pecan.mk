da11c68f42a734ebed1ea8d1171c35c33e7075e4
Ron <cips173@gmail.com>
2012-08-08 11:33:16 +0530

    add initlogo

--- pecan.mk
+++ pecan.mk
@@ -1,20 +1,3 @@
-# Copyright (C) 2011 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# proprietary side of the device
-# Inherit from those products. Most specific first
-
 PRODUCT_AAPT_CONFIG := normal ldpi
 PRODUCT_AAPT_PREF_CONFIG := ldpi
 
@@ -96,6 +79,10 @@ PRODUCT_COPY_FILES += \
     device/lge/pecan/files/ueventd.pecan.rc:root/ueventd.pecan.rc \
     device/lge/pecan/files/init.pecan.usb.rc:root/init.pecan.usb.rc \
     device/lge/pecan/files/init.lge.hidden_reset.sh:root/init.lge.hidden_reset.sh
+    
+# Initlogo
+PRODUCT_COPY_FILES += \
+    device/lge/pecan/prebuilt/initlogo.rle:root/initlogo.rle
 
 # Wifi
 PRODUCT_COPY_FILES += \
@@ -130,7 +117,7 @@ PRODUCT_COPY_FILES += \
 
 # BT startup
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/bin/init.qcom.bt.sh
+    device/lge/pecan/prebuilt/init.qcom.bt.sh:system/bin/init.qcom.bt.sh
 
 # Camera 
 PRODUCT_COPY_FILES += \
