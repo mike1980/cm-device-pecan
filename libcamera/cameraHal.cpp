a0e1da41b5d21836820838411bdcc316a24da049
ROM MARU <keyur2maru@gmail.com>
2012-08-11 20:31:19 +0530

    Changed CameraHAL with LGICS

--- libcamera/cameraHal.cpp
+++ libcamera/cameraHal.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012, Raviprasad V Mummidi.
+ * Copyright (C) 2012, Raviprasad V Mummidi. {LGICS}
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
